8715353b197601d4cdacfb73c84240ef69062dbc
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-05-11 23:14:52 -0600

    init api and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+/**
+ * File which contains the App component.
+ */
+
 import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import AuthForm from './components/Auth/Auth';
 
 const App = () => (
-  <div>
-    APP
-  </div>
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={AuthForm} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
 );
 
 export default App;
